a774c09612a953eddedde868e09bc4f9a49dc517
Vincenzo Tilotta <tailot@gmail.com>
2025-05-12 13:56:32 +0200

    non-blocking loading

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, NavigationStart, RouterOutlet } from '@angular/router';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
 import { LoadingService } from './services/loading.service';
 import { CommonModule } from '@angular/common';
 import { ConsoleOverrideService } from './services/consoleoverride.service';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -17,10 +19,27 @@ import { ConsoleOverrideService } from './services/consoleoverride.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'forge IDEA';
+  private routerSubscription: Subscription | undefined;
 
   constructor(
     public loadingService: LoadingService,
-    private consoleOverrideService: ConsoleOverrideService) { }
+    private consoleOverrideService: ConsoleOverrideService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.routerSubscription = this.router.events.pipe(
+      filter(event => event instanceof NavigationStart)
+    ).subscribe(() => {
+      this.loadingService.hide();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.routerSubscription) {
+      this.routerSubscription.unsubscribe();
+    }
+  }
 }
